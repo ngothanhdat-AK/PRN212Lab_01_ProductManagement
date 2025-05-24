3ab27a36a032e8f04871e0530a05d7facdebdf47
ngothanhdat-AK <ngothanhdat4002@gmail.com>
2025-05-24 22:13:36 +0700

    Update ProductDAO to include additional products
    Enhanced the static constructor of the `ProductDAO` class by adding new `Product` instances: `chef`, `chefMix`, `grandma`, `uncle`, `northwoods`, `mishi`, and `ikura`. The original product list has been commented out, and a new comprehensive list is now assigned to `listProducts`.

--- DataAccessLayer/ProductDAO.cs
+++ DataAccessLayer/ProductDAO.cs
@@ -19,8 +19,19 @@ namespace DataAccessLayer
             Product chai = new Product(1, "Chai", 3, 12, 18);
             Product chang = new Product(2, "Chang", 1, 23, 19);
             Product aniseed = new Product(3, "Aniseed Syrup", 2, 23, 10);
-            listProducts = new List<Product> { chai, chang, aniseed };
-            
+            //listProducts = new List<Product> { chai, chang, aniseed };
+            Product chef = new Product(4, "Chef Anton's Cajun Seasoning", 2, 34, 22);
+            Product chefMix = new Product(5, "Chef Anton's Gumbo Mix", 2, 45, 34);
+            Product grandma = new Product(6, "Grandma's Boysenberry Spread", 2, 21, 25);
+            Product uncle = new Product(7, "Uncle Bob's Organic Dried Pears", 7, 22, 30);
+            Product northwoods = new Product(8, "Northwoods Cranberry Sauce", 2, 10, 40);
+            Product mishi = new Product(9, "Mishi Kobe Niku", 6, 12, 97);
+            Product ikura = new Product(10, "Ikura", 8, 13, 32);
+
+            listProducts = new List<Product> {
+                chai, chang, aniseed, chef, chefMix, grandma, uncle, northwoods, mishi, ikura
+            };
+
         }
 
         public static List<Product> GetProducts()
